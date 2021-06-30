094199bed00056912bb927dd4f6791379692c8d0
Ben Moskovitch <bemoskov@akamai.com>
2021-06-30 10:19:09 +0300

    Chrome update - Added mac support

--- pybenutils/browsers/chrome.py
+++ pybenutils/browsers/chrome.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import sys
 from subprocess import Popen
 from pybenutils.network.download_manager import download_url
@@ -9,17 +10,18 @@ logger = get_logger()
 
 def update_chrome_browser():
     """Updating Chrome browser to its latest version. Returns True if successful, Raise exception on fail"""
-    if sys.platform != 'win32':
-        message = f'The current os {sys.platform} is not yet supported'
-        raise Exception(message)
+    if sys.platform == 'win32':
+        logger.info('Downloading chrome standalone 64 bit version')
+        download_url('https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3'
+                     'D%7B2FBBE98E-4188-4D38-EB51-8DC406611FB1%7D%26lang%3Den%26browser%3D3%26usagestats%3D0%2'
+                     '6appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26insta'
+                     'lldataindex%3Dempty/chrome/install/ChromeStandaloneSetup64.exe')
 
-    logger.info('Downloading chrome standalone 64 bit version')
-    download_url('https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3'
-                 'D%7B2FBBE98E-4188-4D38-EB51-8DC406611FB1%7D%26lang%3Den%26browser%3D3%26usagestats%3D0%2'
-                 '6appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26insta'
-                 'lldataindex%3Dempty/chrome/install/ChromeStandaloneSetup64.exe')
-
-    process = Popen(f'"{os.path.realpath("ChromeStandaloneSetup64.exe")}" /silent /install', shell=True)
-    process.communicate()
-    logger.info('Chrome installer finished')
+        process = Popen(f'"{os.path.realpath("ChromeStandaloneSetup64.exe")}" /silent /install', shell=True)
+        process.communicate()
+        logger.info('Chrome installer finished')
+    else:
+        download_url('https://dl.google.com/dl/chrome/mac/universal/stable/gcea/googlechrome.dmg')
+        subprocess.Popen(r'open /path/to/googlechrome.dmg && cp /volume/googlechrome/Google\ Chrome.app /Applications/',
+                         shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
     return True
